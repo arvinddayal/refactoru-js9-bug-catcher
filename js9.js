c0859d4b2ba41b4a30b2c9de1d52ad27a2ae4246
arvindayal <arvinddayal@gmail.com>
2014-01-22 16:57:01 -0700

    second commit

--- js9.js
+++ js9.js
@@ -1,66 +1,72 @@
-var vicNames = [];
-var vicPhone = [];
-var vicStreet = [];
+var currentUser = null;
 
-var volNames = [];
-var volPhone = [];
-var volStreet = [];
+var login = function (email) {
+  var currentUser = email;
+    };
 
-var displayVics = [];
-var displayVols = [];
+var logout = function () {
+  var currentUser = null;
+    };
 
-var starter = parseInt(prompt("How many disaster victims do you wish to enter?","0"));
+var getLoggedInUser = function () {
+    var id;
+    var result = '';
+    for (id in friends) 
+    {
+    result = result + friends[id];
+    }
 
-for (var i=0; i<starter; i++) {
+    var output = getUserDetails();
 
-	var vicGetNames = prompt("Enter victims name:");
-	vicNames.push(vicGetNames);
+  console.log(output);
 
-	var vicGetPhone = prompt("Enter victims phone number:");
-	vicPhone.push(vicGetPhone);
-
-	var vicGetStreet = prompt("Enter victims street");
-	vicStreet.push(vicGetStreet);
-};
-
-var volunteers = parseInt(prompt("How many volunteers do you wish to enter?","0"));
-
-for (var i=0; i<volunteers; i++) {
-
-	var volGetNames = prompt("Enter volunteers name:");
-	volNames.push(volGetNames);
-
-	var volGetPhone = prompt("Enter volunteers phone number:");		
-	volPhone.push(volGetPhone);
-
-	var volGetStreet = prompt("Enter volunteers street");
-	volStreet.push(volGetStreet);
 };
 
 
-for (var i=0;i<starter;i++) {
-	displayVics.push( "Name: " + vicNames[i] + " Phone: " + vicPhone[i] + " Street: " + vicStreet[i] );
-};
+//   var getUserDetails;(currentUser, function(result){
+// console.log(result);
+//     //print out friends list
 
+//     }
 
-for (var i=0;i<volunteers;i++) {
-	displayVols.push( "Name: " + volNames[i] + " Phone: " + volPhone[i] + " Street: " + volStreet[i] );
-};
-
-alert("Number of persons in need: " + starter +
-	"\nNumber of volunteers: " + volunteers +
-	"\nPersons in need: " + displayVics +
-	"\nVolunteers: " + displayVols);
-
-// console.log(vicNames);
-// console.log(vicPhone);
-// console.log(vicStreet);
-
-// console.log(volNames);
-// console.log(volPhone);
-// console.log(volStreet);
+login('john@doe.com');
+getLoggedInUser();
+logout();
+getLoggedInUser();
 
 
 
 
-// for (var i = 0; i < starter.length; i = starter.length)
+/*********************************/
+/** DO NOT EDIT BELOW THIS LINE **/
+/*********************************/
+
+/**
+ * MOCK - get user details
+ * @param  {string} email
+ * @return {void}
+ */
+function getUserDetails(email, callback){
+  if(email){
+    callback({
+      email: email,
+      username: 'John Doe',
+      friends: [0,1,2,3]
+    });
+  } else {
+    callback(null);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
